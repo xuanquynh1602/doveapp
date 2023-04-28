54c17283f8a5ca3f398359217fa9ca43cd22a3d9
ngochan1712 <132075285+ngochan1712@users.noreply.github.com>
2023-04-28 21:37:44 +0700

    NGOC HAN SUA CODE

--- dove/web1/models.py
+++ dove/web1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username=models.CharField(max_length=30)
+    password=models.CharField(max_length=30)
+    ten=models.CharField(max_length=30)
+
+class Diem(models.Model):
+    diemvan=models.FloatField()
+    diemtoan=models.FloatField()
+    diemhoa=models.FloatField()
+    user=models.ForeignKey(Users,to_field='id',on_delete=models.CASCADE)
+    
+
