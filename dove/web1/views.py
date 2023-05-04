286174b1c07b79426898289a2494e6996d1a35b1
ngochan1712 <132075285+ngochan1712@users.noreply.github.com>
2023-05-04 09:57:00 +0700

    4th5 han

--- dove/web1/views.py
+++ dove/web1/views.py
@@ -13,23 +13,13 @@ def login(request):
 def signup(request):
     if request.method == 'POST':
         registrationForm = RegistrationForm(request.POST)
+        print(registrationForm)
         if registrationForm.is_valid():
             username = registrationForm.cleaned_data['username']
             
-        
+            print(username)
            
     return render(request,'web1/signup.html')
-from django.shortcuts import render
-from .forms import RegistrationForm
-from django.http import HttpResponse
-def index(request):
-    return render(request, 'pages/home.html')
-def contact(request):
-    return render(request,'pages/contact.html')
-def error(request):
-    return render(request,'pages/error.html')
-def register(request):
-    form = RegistrationForm()
 
 
 
